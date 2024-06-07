999b30038fa4760b5d68714c823e72214f9272b2
Fantoca.Tako <fantoca@tako.co.id>
2024-06-07 14:02:18 +0700

    Update footer.tsx

--- components/navbar-footer/footer.tsx
+++ components/navbar-footer/footer.tsx
@@ -26,8 +26,8 @@ const Footer = () => {
 
             <ul className="mt-8 flex gap-6">
               <li>
-                <a
-                  href="#"
+                <Link
+                  href="https://www.facebook.com/profile.php?id=61559844646899"
                   rel="noreferrer"
                   target="_blank"
                   className="text-gray-700 transition hover:opacity-75"
@@ -46,12 +46,12 @@ const Footer = () => {
                       clipRule="evenodd"
                     />
                   </svg>
-                </a>
+                </Link>
               </li>
 
               <li>
-                <a
-                  href="#"
+                <Link
+                  href="https://www.instagram.com/cari_travel/"
                   rel="noreferrer"
                   target="_blank"
                   className="text-gray-700 transition hover:opacity-75"
@@ -70,10 +70,10 @@ const Footer = () => {
                       clipRule="evenodd"
                     />
                   </svg>
-                </a>
+                </Link>
               </li>
 
-              <li>
+              {/* <li>
                 <a
                   href="#"
                   rel="noreferrer"
@@ -91,7 +91,7 @@ const Footer = () => {
                     <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
                   </svg>
                 </a>
-              </li>
+              </li> */}
             </ul>
           </div>
           <div className="w-full grid grid-cols-3 gap-6 lg:gap-8 col-span-2 sm:grid-cols-2 lg:col-span-3 lg:grid-cols-6">
